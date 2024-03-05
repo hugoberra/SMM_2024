536c987647ed1722dff1cb6f942586b7a9fd7442
Erika Xicale <erika.tandemconsultora@outlook.com>
2024-03-05 14:44:40 -0600

    feat: vista tabla municipio
    body: creacion de vistas crud

---
+++ municipios_module/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('ver/', views.ver_municipios, name='ver_municipios'),
+    path('eliminar/<int:id>/', views.eliminar_municipio, name='eliminar_municipio'),
+    path('modificar/<int:id>/', views.modificar_municipio, name='modificar_municipio'),
+]
--- municipios_module/views.py
+++ municipios_module/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Municipality
 
 # Create your views here.
+
+def ver_municipios(request):
+    municipios = Municipality.objects.all()
+    return render(request, 'gestion_municipio_/view_municipality/view_municipality.html', {'municipios': municipios}) #ver_municipios
+
+#def eliminar_municipio(request, id):
+#    municipio = Municipality.objects.get(id=id)
+#    municipio.delete()
+#    return redirect('ver_municipios')
+
+#def modificar_municipio(request, id):
+#    municipio = Municipality.objects.get(id=id)
+#    if request.method == 'POST':
+#        form = MunicipalityForm(request.POST, instance=municipio)
+#        if form.is_valid():
+#            form.save()
+#            return redirect('ver_municipios')
+#    else:
+#        form = MunicipalityForm(instance=municipio)
+#
+#    return render(request, 'gestion_municipio/add_municipality/base.html', {'form': form})
